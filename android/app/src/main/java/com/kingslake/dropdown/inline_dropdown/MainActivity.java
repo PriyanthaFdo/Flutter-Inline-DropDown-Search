b0a28d322d851be882c385561a5f340a0302b2ae
Priyantha.Kingslake <Priyantha.Kingslake@gmail.com>
2023-03-22 09:28:22 +0530

    initial

---
+++ android/app/src/main/java/com/kingslake/dropdown/inline_dropdown/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kingslake.dropdown.inline_dropdown;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:inline_dropdown/dropdownPage.dart';
+
+void main() {
+  runApp(const DropDownRD());
+}
+
+class DropDownRD extends StatelessWidget {
+  const DropDownRD({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Inline Dropdown Research'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          child: const DropDownPage()
+        ),
+      ),
+    );
+  }
+}
